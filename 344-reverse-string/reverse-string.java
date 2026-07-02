c44a0460115957c558d9e59458507142e18f5f0f
Lasya Ragi <lasya2006123@gmail.com>
2026-07-02 19:04:03 +0530

    Time: 4 ms (5.76%) | Memory: 50.4 MB (5.45%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+    //    int i=0;
+    //    int j=s.length-1;
+    //    while(i<j){
+    //     char temp=s[i];
+    //     s[i]=s[j];
+    //     s[j]=temp;
+    //     i++;
+    //     j--;
+    //    }
+     
+
+     Stack<Character>st=new Stack<>();
+
+     for(int i=0;i<s.length;i++){
+        
+        st.push(s[i]);
+     }
+    for(int i=0;i<s.length;i++){
+        s[i]=st.pop();
+    }
+    }
+}
