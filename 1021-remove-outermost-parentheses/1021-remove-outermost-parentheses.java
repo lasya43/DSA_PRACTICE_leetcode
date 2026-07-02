89eac6600aaf0b8cc4d1922579e72755a9307888
Lasya Ragi <lasya2006123@gmail.com>
2026-07-02 10:51:34 +0530

    Time: 4 ms (53.21%), Space: 43 MB (98.36%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,30 +1,49 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         
+         int count=0;
         StringBuilder ans=new StringBuilder();
 
 
-Stack<Character>st=new Stack<>();
+//Stack<Character>st=new Stack<>();
 for(int i=0;i<s.length();i++){
     char ch=s.charAt(i);
 
-    if(ch=='('){
+//     if(ch=='('){
 
-        if(!st.isEmpty()){
-            ans.append(ch);
-        }
-        st.push(ch);
+//         if(!st.isEmpty()){
+//             ans.append(ch);
+//         }
+//         st.push(ch);
+//     }
+//     else{
+//         st.pop();
+
+//         if(!st.isEmpty()){
+//             ans.append(ch);
+//         }
+
+//     }
+// }
+// return ans.toString();
+
+
+if(ch=='('){
+
+    if(count>0){
+        ans.append(ch);
     }
-    else{
-        st.pop();
+    count++;
+}
+else{
 
-        if(!st.isEmpty()){
-            ans.append(ch);
-        }
-
+    count--;
+    if(count>0){
+        ans.append(ch);
     }
 }
-return ans.toString();
 
+}
+return ans.toString();
     }
 }
