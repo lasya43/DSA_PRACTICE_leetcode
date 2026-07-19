e42a0648561ab11fb3556ab3ab98f3229ad84baa
Lasya Ragi <lasya2006123@gmail.com>
2026-07-19 11:02:01 +0530

    Time: 1 ms (77.99%), Space: 42.2 MB (89.56%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,31 @@
+class Solution {
+   
+    static int sqrSum(int n){
+
+            int sum = 0;
+
+            while (n != 0) {
+                int digit = n % 10;
+                sum = sum + (digit * digit);
+                n = n / 10;
+            }
+
+           return sum;
+    }
+
+ public boolean isHappy(int n) {
+    
+int slow=n;
+int fast=n;
+
+do{
+slow=sqrSum(slow);
+fast=sqrSum(sqrSum(fast));
+
+}
+while(slow!=fast);
+
+return slow==1;
+
+}
+}
