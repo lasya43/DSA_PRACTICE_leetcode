e559ad5ce098d58a5a49d404453f5ec3564b54ce
Lasya Ragi <lasya2006123@gmail.com>
2026-07-25 19:01:02 +0530

    Time: 34 ms (61.25%) | Memory: 59.3 MB (32.70%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -23,8 +23,9 @@ class Solution {
                     right--;
 
                     // Skip duplicates for left and right
-                    while (left < right && nums[left] == nums[left - 1]) left++;
+                      while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
+                  
                 } else if (sum < 0) {
                     left++;
                 } else {
